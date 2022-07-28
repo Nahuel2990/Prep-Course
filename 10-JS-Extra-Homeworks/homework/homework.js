1abc834b1369b795cee8a0875cffd24997207fcf
margamon <margaritamonsalve@gmail.com>
2022-07-28 08:56:08 -0500

    Tarea HTML Margarita

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -117,11 +117,11 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí 
-  var arregloComun = [];
-  for(var i = 0; i < arreglo1.length; i++){
-   for(var j = 0; j < arreglo2.length; j++){
-    if (arreglo1[i] === arreglo2[j]){
-         arregloComun.push(arreglo1[i])
+  var arregloComun = [];                       
+  for(var i = 0; i < arreglo1.length; i++){    
+   for(var j = 0; j < arreglo2.length; j++){   
+    if (arreglo1[i] === arreglo2[j]){          
+         arregloComun.push(arreglo1[i])        
     }
    }
   }
